6d7746904f4ef4ad01755da3b23c6c19e1f71290
TuyenBui <duytuyen.bui@tp.com.vn>
2023-05-31 00:17:31 +0700

    doing insert user

--- app/src/main/java/com/example/baseprojectandroid/server/ApiClient.kt
+++ app/src/main/java/com/example/baseprojectandroid/server/ApiClient.kt
@@ -2,12 +2,19 @@ package com.example.baseprojectandroid.server
 
 import com.example.baseprojectandroid.data.api.ResponseModel
 import com.example.baseprojectandroid.model.RegisterBody
+import com.example.baseprojectandroid.model.UploadResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Field
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Path
 import retrofit2.http.Query
 import java.io.File
 
@@ -21,4 +28,16 @@ interface ApiClient {
 
     @POST("/api/Users/insert")
     suspend fun insertUser(@Body registerBody: RegisterBody): ResponseModel
+
+/*
+    @POST("/api/Users/insert")
+    suspend fun insertUser(@Field("name") name: String, @Field("username") username: String, @Field("password") password: String, @Field("image") image: File?): ResponseModel
+*/
+
+    @Multipart
+    @POST("")
+    suspend fun uploadImage(
+        @Part image: MultipartBody.Part,
+        @Part("desc") desc: RequestBody
+    ): Call<UploadResponse>
 }
--- app/src/main/java/com/example/baseprojectandroid/ui/component/splash/getting/signup/name/NameViewModel.kt
+++ app/src/main/java/com/example/baseprojectandroid/ui/component/splash/getting/signup/name/NameViewModel.kt
@@ -6,6 +6,7 @@ import com.example.baseprojectandroid.model.AccountState
 import com.example.baseprojectandroid.model.RegisterBody
 import com.example.baseprojectandroid.server.ApiClient
 import com.example.baseprojectandroid.ui.base.BaseViewModel
+import com.google.gson.Gson
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -28,10 +29,12 @@ class NameViewModel @Inject constructor(
 
     fun callApiRegister(username: String, password: String, nameUser: String, image: File? = null): Flow<AccountState> = flow {
         emit(AccountState.Loading)
-        val response = apiClient.insertUser(RegisterBody(nameUser, username, password, image))
+        val response = apiClient.insertUser(RegisterBody(nameUser, username = username, password = password, image = image))
+//        val response = apiClient.insertUser(nameUser, username, password, image)
         if (response.data.isNullOrEmpty()) {
             emit(AccountState.Failed)
         } else {
+            responseModel = Gson().fromJson(response.data.toString(), ResponseModel::class.java)
             emit(AccountState.Finished)
         }
     }
